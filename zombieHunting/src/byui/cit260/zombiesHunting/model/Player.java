536937b02edac2331d5db49f4b50fd08720c87e9
ChunShing <ChunShing@10.18.147.102>
2014-05-08 16:26:39 -0600

    Initial project commit

---
+++ zombieHunting/src/byui/cit260/zombiesHunting/model/Player.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */ 
+
+package byui.cit260.zombiesHunting.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author ChunShing
+ */
+public class Player implements Serializable{
+    //class instance variables
+    private String name;
+    private double health;
+
+    public Player() {
+    }
+
+    
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHealth() {
+        return health;
+    }
+
+    public void setHealth(double health) {
+        this.health = health;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + ", health=" + health + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + Objects.hashCode(this.name);
+        hash = 23 * hash + (int) (Double.doubleToLongBits(this.health) ^ (Double.doubleToLongBits(this.health) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.health) != Double.doubleToLongBits(other.health)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
