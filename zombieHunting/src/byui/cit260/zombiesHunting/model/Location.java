f7683311352e56dae9e712a1dd1513d430510e99
Computer <Computer@192.168.21.109>
2014-05-10 21:52:58 -0600

    Adds the Location class and tests it

---
+++ zombieHunting/src/byui/cit260/zombiesHunting/model/Location.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.cit260.zombiesHunting.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author Computer
+ */
+public class Location implements Serializable{
+    private String requiredItems;
+    private int coordinates;
+
+    public Location() {
+    }
+
+    public String getRequiredItems() {
+        return requiredItems;
+    }
+
+    public void setRequiredItems(String requiredItems) {
+        this.requiredItems = requiredItems;
+    }
+
+    public int getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(int coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "requiredItems=" + requiredItems + ", coordinates=" + coordinates + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.requiredItems);
+        hash = 29 * hash + this.coordinates;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (!Objects.equals(this.requiredItems, other.requiredItems)) {
+            return false;
+        }
+        if (this.coordinates != other.coordinates) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
