b3c7c6bf716a1decc5f90b1f411d8b4ee1e4fcae
Computer <Computer@Computer-PC>
2014-07-09 13:05:16 -0600

    J frame

--- zombieHunting/src/byui/cit260/zombiesHunting/Exceptions/GameControlExceptions.java
+++ zombieHunting/src/byui/cit260/zombiesHunting/Exceptions/GameControlExceptions.java
@@ -10,6 +10,20 @@ package byui.cit260.zombiesHunting.Exceptions;
  *
  * @author Computer
  */
-public class GameControlExceptions {
+public class GameControlExceptions extends Exception{
+
+    public GameControlExceptions() {
+    }
+        
+    public GameControlExceptions(String message){
+        super(message);
+    }
     
+    public GameControlExceptions(Throwable cause) {
+        super(cause);
+    }
+    
+    public GameControlExceptions(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
